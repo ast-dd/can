3673cf13df86dcd3b3be2c4930a1f3d0e011a798
Stephan Klatt <stephan.klatt@ast.de>
2018-03-28 12:00:25 +0200

    TEST ReadWriteCloser: remove buf and Read(), rename writeSocket to writeFd

--- test_readwritecloser.go
+++ test_readwritecloser.go
@@ -1,16 +1,12 @@
 package can
 
 import (
-	"bytes"
-	"io"
-	"time"
 	"syscall"
 )
 
 type echoReadWriteCloser struct {
-	buf         bytes.Buffer
-	closed      bool
-	writeSocket int
+	closed  bool
+	writeFd int
 }
 
 // NewEchoReadWriteCloser returns a ReadWriteCloser which echoes received bytes
@@ -20,27 +16,15 @@ func NewEchoReadWriteCloser() ReadWriteCloser {
 	if err != nil {
 		panic(err)
 	}
-	return NewReadWriteCloser(&echoReadWriteCloser{writeSocket: pair[0]}, pair[1])
+	return NewReadWriteCloser(&echoReadWriteCloser{writeFd: pair[0]}, pair[1])
 }
 
 func (rw *echoReadWriteCloser) Read(b []byte) (n int, err error) {
-	for {
-		if rw.buf.Len() > 0 {
-			return rw.buf.Read(b)
-		}
-
-		if rw.closed == true {
-			break
-		}
-
-		<-time.After(time.Millisecond * 1)
-	}
-
-	return 0, io.EOF
+	panic("Read() shouldn't be called anymore")
 }
 
 func (rw *echoReadWriteCloser) Write(b []byte) (n int, err error) {
-	err = syscall.Sendmsg(rw.writeSocket, b, nil, nil, 0)
+	err = syscall.Sendmsg(rw.writeFd, b, nil, nil, 0)
 	n = len(b)
 
 	return
